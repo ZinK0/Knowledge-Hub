55cf64d30d398d67a8ae3da0dc885d87f4f544a5
Zin KO <zinko.it@outlook.com>
2024-10-05 15:56:28 +0630

    Success Looping the filtered array

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -9,6 +9,58 @@ console.log(firstARTICLE.imgUrl);
 console.log(articles);
 console.log(filteredArticles);
 
+function renderArticleCard(articles) {
+  articles.forEach((article) => {
+    let tableRow = $("<tr>");
+    let tableHeadingContainer = $("<th>").attr("scope", "row");
+    let tableListHeading = $("<h5>").text(`${article.postID}`);
+    let tableListBodyContainer = $("<div>").addClass(
+      "card border-0 p-2 flex-row"
+    );
+    let tableListBodyImg = $("<img>")
+      .addClass("rounded object-fit-cover col-4")
+      .attr("src", article.imgUrl);
+    let tableListCard = $("<div>").addClass("card-body col-8");
+    let tableListCardTitle = $("<h5>")
+      .addClass("card-title")
+      .text(`${article.title}`);
+    let tableListCardContents = $("<p>")
+      .addClass("card-text text-black-70")
+      .text(`${article.contents}`);
+    let tableListDate = $("<td>").text(`${article.publishedDate}`);
+    let tableListAction = $("<td>");
+    let tableListActionContainer = $("<div>").addClass(
+      "d-flex flex-column w-50 h-auto"
+    );
+    let tableListEditActionButton = $("<button>")
+      .addClass("btn btn-primary mb-2")
+      .text("Edit");
+    let tableListDeleteActionButton = $("<button>")
+      .addClass("btn btn-danger")
+      .text("Delete");
+
+    tableListCard.append(tableListCardTitle, tableListCardContents);
+
+    tableListBodyContainer.append(tableListBodyImg, tableListCard);
+    tableHeadingContainer.append(tableListHeading, tableListBodyContainer);
+
+    tableListActionContainer.append(
+      tableListEditActionButton,
+      tableListDeleteActionButton
+    );
+
+    tableRow.append(
+      tableHeadingContainer,
+      tableListDate,
+      tableListActionContainer
+    );
+
+    $(".table-group-divider").append(tableRow);
+  });
+}
+
+renderArticleCard(filteredArticles);
+
 // Create Table Row
 console.log("Profile JS Loaded");
 
